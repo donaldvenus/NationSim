920c3b774b0c260714c31017b2c2956a5162ba1b
Donald Venus <donaldvenus120@gmail.com>
2018-07-19 01:33:35 -0400

    Add borders structure and basic combat

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 	var canvas = document.getElementById('sim-box');
 	var ctx = canvas.getContext('2d');
 	var nationInfo = document.getElementById('nation-info');
+	var nationCount = 16;
+	var dxdy = [[-1,0],[1,0],[0,-1],[0,1]]; // Only support 4 direction movement
+	var paused = true;
 
 	/* Add click handler for canvas. */
 	canvas.addEventListener('click', function(event) {
@@ -13,7 +16,8 @@
 	    selectedNation = nationsArr[grid[gridY][gridX].ownerIndex];
 	    nationInfo.innerHTML = selectedNation.name + '<br>' + 
 	                           'Population: ' + selectedNation.population + 
-	                           '<br>' + 'Economy: ' + selectedNation.econValue;
+	                           '<br>' + 'Economy: ' + selectedNation.econValue +
+	                           '<br>' + 'Military: ' + selectedNation.military; 
 	}, false);
 
 	/** 
@@ -33,11 +37,7 @@
 	                'Maroon','Mint','Olive','Coral','Navy','Grey','White',
 	                'Black'];
 	var nationsArr = [];
-	var grid = generateGrid();
-	var selectedNation = undefined;
-	createNations(8);
-	initNations();
-	simLoop();
+	resetGame();
 
 	/**
 	 * Generates the grid for the simulation by creating a province for every grid
@@ -65,16 +65,24 @@
 		province.econValue = Math.floor(Math.random() * 10);
 		province.ownerIndex = -1;
 		province.isCapital = 0;
+		province.borders = [];
+		province.borders.length = nationCount;
+		province.borders.fill(0);
 		return province;
 	}
 
 	// Generates a new nation given an index for color and name
 	function generateNation(index) {
 		var nation = {};
+		nation.ownerIndex = index;
 		nation.color = colorsArr[index];
 		nation.name = namesArr[index];
 		nation.population = 0;
 		nation.econValue = 0;
+		nation.military = 0;
+		nation.threats = [];
+		nation.borders = [];
+		nation.committedMilitary = [];
 		do {
 			nation.capital = grid[Math.floor(Math.random() * 40)][Math.floor(Math.random() * 80)];
 		}
@@ -89,24 +97,33 @@
 		for (var i = 0; i < numNations; i++) {
 			var nation = generateNation(i);
 			nation.capital.ownerIndex = i;
+			nation.threats.length = numNations;
+			nation.threats.fill(-1);
+			nation.borders.length = numNations;
+			nation.borders.fill(new Set());
 			fillqueue.push(nation.capital);
 			nationsArr.push(nation);
 		}
-		var dx = [-1, 0, 1];
-		var dy = [-1, 0, 1];
 		while (fillqueue.length !== 0) {
 			if (Math.floor(Math.random() * 2) !== 0) {
 				fillqueue.push(fillqueue.shift());
 				continue;
 			}
 			var curr = fillqueue.shift();
-			for (var x = 0; x < 3; x++) {
-				for (var y = 0; y < 3; y++) {
-					var prov = getProvince(curr.x + dx[x], curr.y + dy[y]);
-					if (prov !== undefined && prov.ownerIndex === -1) {
-						prov.ownerIndex = curr.ownerIndex;
-						fillqueue.push(prov);
-					}
+			for (var xy = 0; xy < dxdy.length; xy++) {
+				var prov = getProvince(curr.x + dxdy[xy][0], curr.y + dxdy[xy][1]);
+				if (prov === undefined) continue;
+				if (prov.ownerIndex === -1) {
+					prov.ownerIndex = curr.ownerIndex;
+					fillqueue.push(prov);
+				}
+				else if (prov.ownerIndex !== curr.ownerIndex) {
+					// Might not need both...
+					nationsArr[curr.ownerIndex].borders[prov.ownerIndex].add(curr);
+					nationsArr[prov.ownerIndex].borders[curr.ownerIndex].add(prov);
+					prov.borders[curr.ownerIndex]++;
+					curr.borders[prov.ownerIndex]++;
+					nationsArr[curr.ownerIndex].threats[prov.ownerIndex] = nationsArr[prov.ownerIndex].military;
 				}
 			}
 		}
@@ -118,8 +135,16 @@
 			for (var j = 0; j < numCols; j++) {
 				nationsArr[grid[i][j].ownerIndex].population 
 					+= grid[i][j].population;
+				nationsArr[grid[i][j].ownerIndex].military 
+					+= grid[i][j].population + (grid[i][j].econValue * 10);
 				nationsArr[grid[i][j].ownerIndex].econValue
 					+= grid[i][j].econValue;
+			}
+		}
+		for (var i = 0; i < nationsArr.length; i++) {
+			for (var j = 0; j < nationsArr[i].threats.length; j++) {
+				if (nationsArr[i].threats[j] === 0)
+					nationsArr[i].threats[j] = nationsArr[j].military;
 			}
 		}
 	}
@@ -138,13 +163,157 @@
 				ctx.fillRect(j * 10, i * 10, 10, 10);
 			}
 		}
+		if (selectedNation) {
+			nationInfo.innerHTML = selectedNation.name + '<br>' + 
+	                           'Population: ' + selectedNation.population + 
+	                           '<br>' + 'Economy: ' + selectedNation.econValue +
+	                           '<br>' + 'Military: ' + selectedNation.military;
+	    } 
+	}
+
+	// Commit military to threatening nations.
+	function commitMilitary() {
+		for (var i = 0; i < nationsArr.length; i++) {
+			var totalThreat = 0;
+			var nation = nationsArr[i];
+			for (var j = 0; j < nation.threats.length; j++) {
+				if (i === j) continue;
+				if (nation.threats[j] === -1) continue;
+				totalThreat += nation.threats[j];
+			}
+			for (var j = 0; j < nation.threats.length; j++) {
+				if (i === j) continue;
+				if (nation.threats[j] === -1) continue;
+				nation.committedMilitary[j] = nation.military * nation.threats[j] / totalThreat;
+			}
+		}
+	}
+
+	// Resolve conflict between nations with committed military forces.
+	function resolveConflict() {
+		for (var i = 0; i < nationsArr.length; i++) {
+			for (var j = i + 1; j < nationsArr.length; j++) {
+				var nation1 = nationsArr[i];
+				var nation2 = nationsArr[j];
+				if (nation1.threats[j] === -1) continue;
+				var military1 = Math.floor(nation1.committedMilitary[j] * (Math.random()));
+				var military2 = Math.floor(nation2.committedMilitary[i] * (Math.random()));
+				if (military1 > military2) winConflict(nation1, nation2, military1, military2);
+				if (military1 === military2) continue;
+				if (military1 < military2) winConflict(nation2, nation1, military2, military1);
+			}
+		}
+	}
+
+	// Handle border adjustment after conflict.
+	function winConflict(winner, loser, winnerForce, loserForce) {
+		// Add changes to military value
+		// TODO mark which provinces are borders to which nations so these values
+		// can easily be updated as they change hands.
+		// Use sets to keep track of borders
+		// Remove invalid borders from loser 
+		// Maybe track internal borders instead of external?
+		var loss = Math.min(winnerForce - loserForce, loserForce);
+		loser.military = loser.military - loss;
+		winner.military = winner.military - Math.floor(loss / 2);
+		if (loser.military < 1) loser.military = 1;
+		if (winner.military < 1) winner.military = 1;
+		var oldBorders = new Set(winner.borders[loser.ownerIndex]);
+		oldBorders.forEach(function(curr) {
+			// curr is the interal border provinces of the winner
+			// prov is a bordering province of a curr
+			if (Math.floor(Math.random() * 2) !== 0) {
+				curr.borders[loser.ownerIndex] = 0;
+				for (var xy = 0; xy < dxdy.length; xy++) {
+					var prov = getProvince(curr.x + dxdy[xy][0], curr.y + dxdy[xy][1]);
+					if (prov === undefined) continue;
+					if (prov.ownerIndex === loser.ownerIndex) {
+						prov.ownerIndex = winner.ownerIndex;
+						adjustBorders(loser, prov);
+					}
+				}
+			}
+		});
+	}
+
+	// Fixes borders when a province changes hands
+	function adjustBorders(loser, prov) {
+		// Economic changes
+		loser.econValue -= prov.econValue;
+		loser.population -= prov.population;
+		nationsArr[prov.ownerIndex].econValue += prov.econValue;
+		nationsArr[prov.ownerIndex].population += prov.population;
+
+		// Remove lost province from any of loser's borders and reset province
+		// to have no borders (will be determined in next step).
+		for (var i = 0; i < prov.borders.length; i++) {
+			if (prov.borders[i] > 0) {
+				loser.borders[i].delete(prov);
+			}
+			prov.borders[i] = 0;
+		}
+		// winner borders
+		// loser borders
+		// province borders
+		// adjacent borders + third party nation borders
+		// TODO remove border between adjacent and loser if necessary
+		// Look at adjacent provinces and determine any other changes.
+		for (var xy = 0; xy < dxdy.length; xy++) {
+			var adj = getProvince(prov.x + dxdy[xy][0], prov.y + dxdy[xy][1]);
+			if (adj === undefined) continue;
+			// If owned by winner, do nothing.
+			if (adj.ownerIndex === prov.ownerIndex) continue;
+			// adj borders new owner and no longer old owner
+			// prov borders owner of adjacent
+			adj.borders[loser.ownerIndex]--;
+			adj.borders[prov.ownerIndex]++;
+			prov.borders[adj.ownerIndex]++;
+
+			// adjacent nation gains border with new province holder and loses
+			// border with old owner if last border was lost.
+			nationsArr[adj.ownerIndex].borders[prov.ownerIndex].add(adj);
+			// If adj no longer borders loser, remove province from owner's borders for the loser
+			if (adj.borders[loser.ownerIndex] === 0) {
+				nationsArr[adj.ownerIndex].borders[loser.ownerIndex].delete(adj);
+				// If this was the last border, remove threat of loser to adj's owner.
+				if (nationsArr[adj.ownerIndex].borders[loser.ownerIndex].empty) 
+					nationsArr[adj.ownerIndex].threats[loser.ownerIndex] = -1;
+			}
+			// owner of prov gains border with adjacent's owner and associated
+			// threat level. Owner of adj gains threat with new owner of prov
+			nationsArr[prov.ownerIndex].borders[adj.ownerIndex].add(prov);
+			nationsArr[adj.ownerIndex].threats[prov.ownerIndex] = nationsArr[prov.ownerIndex].military;
+			nationsArr[prov.ownerIndex].threats[adj.ownerIndex] = nationsArr[adj.ownerIndex].military;
+		}
+	}
+
+	// Every nation creates new forces for war.
+	function develop() {
+		for (var i = 0; i < nationsArr.length; i++) {
+			nationsArr[i].military += Math.floor(nationsArr[i].econValue / 10) + Math.floor(nationsArr[i].population / 100);
+		}
+	}
+
+	function resetGame() {
+		grid = generateGrid();
+		selectedNation = undefined;
+		createNations(nationCount);
+		initNations();
+		paused = true;
+		drawMap();
+		simLoop();
 	}
 
 	// Runs the simulation until stopped.
 	function simLoop() {
-		ctx.clearRect(0,0,800,400);
-		drawMap();
-		window.requestAnimationFrame(simLoop);
+		if (!paused) {
+			ctx.clearRect(0,0,800,400);
+			commitMilitary();
+			resolveConflict();
+			develop();
+			drawMap();
+		}
+		setTimeout(simLoop, 500);
 	}
 
 })();
